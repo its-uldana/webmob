dd82fed5d1bc67ac482d1f763b32d8438549132b
Uldana <26035@almau.edu.kz>
2024-11-01 09:13:09 +0500

    step18 use stateful component

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -1,25 +1,33 @@
-import React from 'react';
+import React, {Component} from 'react';
+import './todo-list-item.css';
 
-import TodoListItem from './todo-list-item';
-import './todo-list.css';
+export default class TodoListItem extends Component {
+    render () {
 
-const TodoList = ({ todos }) => {
-
-    const elements = todos.map((item) => {
-        const { id, ...itemProps } = item;
-
+        const { label, important = false } = this.props;
+        const style = {
+            color: important ? 'steelblue' : 'black',
+            fontWeight: important ? 'bold' : 'normal'
+        };
         return (
-            <li key={id} className="list-group-item">
-                <TodoListItem {...itemProps } />
-            </li>
+            <span className="todo-list-item">
+      <span
+          className="todo-list-item-label"
+          style={style}>
+        {label}
+      </span>
+
+      <button type="button"
+              className="btn btn-outline-success btn-sm float-right">
+        <i className="fa fa-exclamation" />
+      </button>
+
+      <button type="button"
+              className="btn btn-outline-danger btn-sm float-right">
+        <i className="fa fa-trash-o" />
+      </button>
+    </span>
         );
-    });
+    };
 
-    return (
-        <ul className="list-group todo-list">
-            { elements }
-        </ul>
-    );
-};
-
-export default TodoList;
+}
